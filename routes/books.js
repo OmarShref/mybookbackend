9d020786b190954029370f6b480a21c9bc447ea2
Omar Sharif <mohandesomar@gmail.com>
2023-03-10 16:12:48 +0200

    first commit

---
+++ routes/books.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const router = express.Router();
+const Book = require("../schemas/Book");
+const joi = require("joi");
+
+// GET
+router.get("/", (req, res) => {
+  Book.find({})
+    .then((data) => res.status(200).send(data))
+    .catch((err) => res.status(204).send());
+});
+
+// POST
+router.post("/", (req, res) => {
+  const body = req.body;
+  const schema = joi.object({
+    title: joi.string().trim().required(),
+    author: joi.string().trim().required(),
+    genre: joi.string().trim().required(),
+  });
+  const { error } = schema.validate(body);
+  if (error) {
+    res.status(200).send(error);
+  } else {
+    const newBook = {
+      title: body.title,
+      author: body.author,
+      genre: body.genre,
+    };
+    Book.insertMany(newBook)
+      .then(() => res.status(201).send())
+      .catch((err) => res.status(500).send(err));
+  }
+});
+
+// DELETE
+router.delete("/", (req, res) => {
+  const body = req.body;
+  const schema = joi.object({
+    id: joi.string().trim().required(),
+  });
+  const { error } = schema.validate(body);
+  if (error) {
+    res.status(204).send();
+  } else {
+    Book.findOneAndDelete({ _id: body.id })
+      .then(() => {
+        res.status(200).send();
+      })
+      .catch(() => res.status(204).send());
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+// starting server
+const app = express();
+// setting the port
+const port = process.env.PORT || 7000;
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+    credentials: true,
+  })
+);
+// Mongodb Connect
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.DB_STRING, {})
+  .then(console.log("connected to db successfully"))
+  .catch((err) => console.log(err));
+
+// Routes
+const booksRouter = require("./routes/books");
+app.use("/", booksRouter);
+
+app.listen(port);
+console.log(`server is working on : http://localhost:${port}`);
